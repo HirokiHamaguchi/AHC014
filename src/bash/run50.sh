df0257149f7ae93e7ca523dd88298a98e5a354c7
hari64 <hari64boli64@gmail.com>
2022-09-21 14:49:05 +0900

    48057769 (3469ms)

--- src/bash/run50.sh
+++ src/bash/run50.sh
@@ -23,17 +23,19 @@ if [ -f $F_BESTSCORES ]; then
     IFS=$'\n'
 fi
 
+TIMEFORMAT=$' | time:%3lR'
+
 declare -A nowScores
 for seed in $(seq -f '%04g' 0 $seedNum); do
     echo "seed:$seed"
-    score=$(./src/cpp/solve < input/$seed.txt)
+    time { score=$(./src/cpp/solve < input/$seed.txt 2> output/$seed.txt); }
     prev="${prevScores[${seed}]}"
     if [ $prev != "" ]; then
         percent=`python3 -c "print($score/$prev*100)"`%
     else
         percent="no prev"
     fi
-    echo " score:$score ($percent)"
+    echo " |score:$score ($percent)"
     echo $seed=$score >> $F_OUT
     nowScores[$seed]=$score
 done
---
+++ src/python/sum.py
@@ -0,0 +1,11 @@
+scores = dict()
+with open("src/bash/bestScores.log", mode="r") as f:
+    while True:
+        s = f.readline()
+        if s:
+            seed, score = map(int, s.split("="))
+            scores[seed] = score
+        else:
+            break
+print(scores)
+print(sum(scores.values()))
