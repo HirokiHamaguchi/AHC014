1a4a537c4ed843670d1c72b2f20d102c68ef9e68
hari64 <hari64boli64@gmail.com>
2022-09-25 13:18:14 +0900

    update profile.sh

--- src/bash/profile.sh
+++ src/bash/profile.sh
@@ -1,6 +1,6 @@
 echo "now compiling..."
 /usr/bin/g++ -pg -g -fdiagnostics-color=always ./src/cpp/solve.cpp -o ./src/cpp/solve -std=c++17 -Wall -Wextra -O3
 echo "now running..."
-./src/cpp/solve < input/0004.txt
+./src/cpp/solve < input/0006.txt
 gprof ./src/cpp/solve gmon.out # -A
 rm gmon.out
