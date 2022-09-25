b351f752840f950953954ba5090d954d8f4d88fc
hari64 <hari64boli64@gmail.com>
2022-09-25 10:23:41 +0900

    update bash commands

--- src/bash/profile.sh
+++ src/bash/profile.sh
@@ -1,6 +1,6 @@
 echo "now compiling..."
 /usr/bin/g++ -pg -g -fdiagnostics-color=always ./src/cpp/solve.cpp -o ./src/cpp/solve -std=c++17 -Wall -Wextra -O3
 echo "now running..."
-./src/cpp/solve < input/0000.txt
+./src/cpp/solve < input/0004.txt
 gprof ./src/cpp/solve gmon.out # -A
 rm gmon.out
---
+++ src/bash/run500.sh
@@ -0,0 +1,32 @@
+set -e
+
+echo "now compiling..."
+/usr/bin/g++ -fdiagnostics-color=always ./src/cpp/solve.cpp -o ./src/cpp/solve -std=c++17 -Wall -Wextra -O3 -DTEST
+
+YELLOW="\033[33m"
+RESET="\033[0m"
+FOLDER="./src/bash/"
+F_OUT="${FOLDER}out500.log"
+
+seedNum=499 #500-1
+
+echo -n >| $F_OUT
+
+TIMEFORMAT=$' | time:%3lR'
+
+declare -A nowScores
+for seed in $(seq -f '%04g' 0 $seedNum); do
+    echo "seed:$seed"
+    echo "seed:$seed" >> $F_OUT
+    time { score=$(./src/cpp/solve < input2/$seed.txt 2> /dev/null); }
+    echo " |score:$score"
+    echo " |score:$score" >> $F_OUT
+    nowScores[$seed]=$score
+done
+
+avg=0
+for score in ${nowScores[@]}; do
+   ((avg+=score))
+done
+echo avg: $avg
+echo avg: $avg >> $F_OUT
